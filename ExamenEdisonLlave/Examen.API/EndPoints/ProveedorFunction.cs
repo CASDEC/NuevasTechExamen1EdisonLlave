84169eb69f88c220db3feefb033f66c601d47610
Edison Llave <edisonllave@gmail.com>
2024-03-20 21:45:18 -0400

    Proveedor Finalizado

---
+++ ExamenEdisonLlave/Examen.API/EndPoints/ProveedorFunction.cs
@@ -0,0 +1,106 @@
+using Examen.API.Contratos.Repositorios;
+using Examen.API.Modelo;
+using Examen.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.Net;
+
+namespace Examen.API.EndPoints
+{
+    public class ProveedorFunction
+    {
+        private readonly ILogger<ProveedorFunction> _logger;
+        private readonly IRepositorio _repositorio;
+
+        public ProveedorFunction(ILogger<ProveedorFunction> logger, IRepositorio repositorio)
+        {
+            _logger = logger;
+            _repositorio = repositorio;
+        }
+
+        [Function("ListarProveedor")]
+        [OpenApiOperation("Proveedorlist", "Proveedor", Description = "Listar Proveedor")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IProveedor), Description = "Lista de Proveedores")]
+        public async Task<HttpResponseData> ListarProveedor([HttpTrigger(AuthorizationLevel.Function, "get", Route = "ListarProveedor")] HttpRequestData req)
+        {
+            _logger.LogInformation("ListarProveedor");
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(_repositorio.ListarTodos<Proveedor>());
+            return res;
+
+        }
+        [OpenApiOperation("Proveedorinser", "Proveedor", Description = "Insertar Proveedor")]
+        [OpenApiRequestBody("application/json", typeof(IProveedor), Description = "Inserte Proveedor")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IProveedor), Description = "Proveedor Insertado")]
+
+        [Function("InsertarProveedor")]
+         public async Task<HttpResponseData> InsertarProveedor([HttpTrigger(AuthorizationLevel.Function, "post", Route = "InsertarProveedor")] HttpRequestData req)
+        {
+            _logger.LogInformation("InsertarProveedor");
+            var data = await req.ReadFromJsonAsync<Proveedor>();
+            Proveedor success = await _repositorio.Insertar<Proveedor>(data);
+            if (success != null)
+            {
+                var respuesta = req.CreateResponse(HttpStatusCode.OK);
+                return respuesta;
+            }
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+        [Function("ActualizarProveedor")]
+        [OpenApiOperation("Proveedoractu", "Proveedor", Description = "Actualizar Proveedor")]
+        [OpenApiRequestBody("application/json", typeof(IProveedor), Description = "Actualizar Proveedor")]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "ID del Proveedor")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IProveedor), Description = "Proveedor Actualizado")]
+
+        public async Task<HttpResponseData> ActualizarProveedor([HttpTrigger(AuthorizationLevel.Function, "put", Route = "ActualizarProveedor/{id}")] HttpRequestData req, string id)
+        {
+            _logger.LogInformation("ActualizarProveedor");
+            var data = await req.ReadFromJsonAsync<Proveedor>();
+            data.RowKey = id;
+            bool success = await _repositorio.Actualizar<Proveedor>(data);
+            if (success)
+            {
+                var respuesta = req.CreateResponse(HttpStatusCode.OK);
+                return respuesta;
+            }
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+        [Function("EliminarProveedor")]
+        [OpenApiOperation("Proveedorelim", "Proveedor", Description = "Eliminar Proveedor")]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "ID del Proveedor")]
+        [OpenApiResponseWithoutBody(HttpStatusCode.OK ,Description = "Proveedor Eliminado")]
+
+        public async Task<HttpResponseData> EliminarProveedor([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "EliminarProveedor/{id}")] HttpRequestData req, string id)
+        {
+            _logger.LogInformation("EliminarProveedor");
+            var data = new Proveedor();
+            data.RowKey = id;
+            bool success = await _repositorio.Eliminar<Proveedor>(data);
+            if (success)
+            {
+                var respuesta = req.CreateResponse(HttpStatusCode.OK);
+                return respuesta;
+            }
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        [Function("ObtenerProveedor")]
+        [OpenApiOperation("Proveedorobt", "Proveedor", Description = "Obtener Proveedor")]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "ID del Proveedor")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IProveedor), Description = "Obtener Proveedor")]
+        public async Task<HttpResponseData> ObtenerProveedor([HttpTrigger(AuthorizationLevel.Function, "get", Route = "ObtenerProveedor/{id}")] HttpRequestData req, string id)
+        {
+            _logger.LogInformation("ObtenerProveedor");
+            var data = new Proveedor();
+            data.RowKey = id;
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(_repositorio.ListarUno<Proveedor>(data));
+            return res;
+        }
+    }
+}
--- ExamenEdisonLlave/Examen.API/Program.cs
+++ ExamenEdisonLlave/Examen.API/Program.cs
@@ -1,13 +1,28 @@
+using Examen.API.Contratos.Repositorios;
+using Examen.API.Implementacion.Repositorios;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
+        var configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+           .AddEnvironmentVariables()
+           .Build();
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IRepositorio, Repositorio>((s) =>
+        {
+            var loggerFactory = s.GetRequiredService<ILoggerFactory>();
+            string cadenaConexion = configuration.GetConnectionString("cadena");
+            return new Repositorio(cadenaConexion);
+        });
     })
     .Build();
 
